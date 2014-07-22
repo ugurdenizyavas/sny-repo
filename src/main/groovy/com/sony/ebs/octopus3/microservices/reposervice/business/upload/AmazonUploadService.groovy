d28cd6edf3b3dd4630e58f1484a1343d4d90eee7
tryavasu <ugurdeniz.yavas@eu.sony.com>
2014-07-22 15:22:11 +0300

    Commons ratpack is added+ Amazon Upload Service is groovyfied

--- src/main/groovy/com/sony/ebs/octopus3/microservices/reposervice/business/upload/AmazonUploadService.groovy
+++ src/main/groovy/com/sony/ebs/octopus3/microservices/reposervice/business/upload/AmazonUploadService.groovy
@@ -4,8 +4,7 @@ import com.amazonaws.ClientConfiguration
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.PutObjectRequest
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
@@ -13,10 +12,9 @@ import org.springframework.stereotype.Component
  * @author ferhat sobay
  * @author tryavasu
  */
+@Slf4j
 @Component
 class AmazonUploadService {
-
-    private static final Logger logger = LoggerFactory.getLogger(AmazonUploadService.class)
 
     final def accessKeyId = "AKIAJDSMP4FH74HDUXYQ"
     final
@@ -24,7 +22,6 @@ class AmazonUploadService {
     final def bucketName = "vendorfeeds-uk"
     final def uploadPath = "sony-products/dropbox/UK"
 
-    //TODO: Clarify these params with TCS
     @Value('${amazon.s3.proxyHost}')
     String proxyHostParam
     @Value('${amazon.s3.proxyPort}')
@@ -37,31 +34,25 @@ class AmazonUploadService {
     Integer connectionTimeoutParam
 
     void upload(file, destination) {
+
         //TODO: Add multi destination feature
-        def clientConfiguration = new ClientConfiguration()
-
-        clientConfiguration.with {
-            if (proxyHostParam) setProxyHost(proxyHostParam)
-            if (proxyPortParam) setProxyPort(proxyPortParam)
-            if (proxyUsernameParam) setProxyUsername(proxyUsernameParam)
-            if (proxyUsernameParam) setProxyPassword(proxyPasswordParam)
-            if (connectionTimeoutParam) setConnectionTimeout(connectionTimeoutParam)
-        }
-
-        def myCredentials = new BasicAWSCredentials(
-                accessKeyId, secretAccessKey)
-
-        def s3client = new AmazonS3Client(myCredentials, clientConfiguration)
-
-        s3client.with {
+        new AmazonS3Client(new BasicAWSCredentials(accessKeyId, secretAccessKey),
+                new ClientConfiguration(
+                        proxyHost: proxyHostParam,
+                        proxyPort: proxyPortParam,
+                        proxyUsername: proxyUsernameParam,
+                        proxyPassword: proxyPasswordParam,
+                        connectionTimeout: connectionTimeoutParam
+                )
+        ).with {
             try {
                 putObject(new PutObjectRequest(bucketName, uploadPath + File.separator + file.name, file))
             } catch (Exception e) {
-                logger.error("Unexpected error occurred while uploading amazon images. Exception", e)
                 return false
             }
 
             return true
         }
+
     }
 }
