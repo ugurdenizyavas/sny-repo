84e5a3196d914c34d9b5e4f738d1ed15c933edb5
tryavasu <ugurdeniz.yavas@eu.sony.com>
2014-08-12 11:09:35 +0300

    New tests are added

--- src/test/groovy/com/sony/ebs/octopus3/microservices/reposervice/test/OpsTest.groovy
+++ src/test/groovy/com/sony/ebs/octopus3/microservices/reposervice/test/OpsTest.groovy
@@ -20,6 +20,16 @@ class OpsTest {
         assert result[0], new Operation(methodName: OperationEnum.ZIP, parameters: [source: "flix_sku:global:en_gb"])
         assert result[1], new Operation(methodName: OperationEnum.COPY, parameters: [source: "flix_sku:global:en_gb", destination: "urn:archive:flix_sku:global:en_gb.zip"])
         assert result[2], new Operation(methodName: OperationEnum.UPLOAD, parameters: [source: "flix_sku:global:en_gb", destination: "S3"])
+        assert result[3], new Operation(methodName: OperationEnum.DELETE, parameters: [source: "flix_sku:global:en_gb"])
+    }
+
+    @Test
+    void testOperations2() {
+        def result = OpsParser.parse(new ClassPathResource("ops/ops2.json").file.text)
+
+        assert result.size(), 3
+        assert result[0], new Operation(methodName: OperationEnum.ZIP, parameters: [source: "flix_sku:global:en_gb"])
+        assert result[1], new Operation(methodName: OperationEnum.COPY, parameters: [source: "flix_sku:global:en_gb", destination: "urn:archive:flix_sku:global:en_gb.zip"])
         assert result[2], new Operation(methodName: OperationEnum.DELETE, parameters: [source: "flix_sku:global:en_gb"])
     }
 }
